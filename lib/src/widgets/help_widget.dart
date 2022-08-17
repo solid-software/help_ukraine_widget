695ddc9ecc9cd83e7f13518e95c2cc93bf543d3a
trixee <trixeenya@gmail.com>
2022-08-17 10:44:30 +0300

    Refactor HelpWidget

--- lib/src/widgets/help_widget.dart
+++ lib/src/widgets/help_widget.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:help_ukraine_widget/help_ukraine_widget.dart';
 
+/// A builder a function type for main card of widget.
+typedef MainCardBuilder = Widget Function(_MainBuilderController);
+
+/// A builder a function type for options card of widget.
+typedef OptionsBuilder = Widget Function(_OptionsBuilderController);
+
+class _MainBuilderController {
+  VoidCallback onDetails = () {
+    return;
+  };
+  VoidCallback onClose = () {
+    return;
+  };
+}
+
+class _OptionsBuilderController {
+  VoidCallback onClose = () {
+    return;
+  };
+  List<HelpOptionButton> options = [];
+}
+
+/// It's a widget that displays a flag, and when you click on it, it displays
+/// a card with a title and a description, and when you click on the
+/// description, it displays a card with a list of options.
+///
+/// Has a axis parameter to define direction of animation.
+/// And has 2 builders [MainCardBuilder] and [OptionsBuilder] that you can
+/// use to create your own [HelpWidget].
 class HelpWidget extends StatefulWidget {
+  /// A parameter that defines the direction of the animation.
   final Axis axis;
 
-  final String mainCardTitle;
-  final String? mainCardDescription;
-
+  /// It's a list of buttons that will be displayed in the options card.
   final List<HelpOptionButton> options;
 
-  final Widget Function(
-    Widget flag,
-    String title,
-    String? description,
-    VoidCallback onDetailsCallback,
-    VoidCallback onClose,
-  ) mainCardBuilder;
+  /// It's a builder a function type for main card of widget.
+  final MainCardBuilder mainCardBuilder;
 
-  final Widget Function(
-    List<HelpOptionButton>,
-    VoidCallback onClose,
-  ) optionsCardBuilder;
+  /// It's a builder a function type for options card of widget.
+  final OptionsBuilder optionsCardBuilder;
 
   ///Constructor
   const HelpWidget({
     Key? key,
     this.axis = Axis.vertical,
     required this.mainCardBuilder,
-    required this.mainCardTitle,
     required this.options,
     required this.optionsCardBuilder,
-    this.mainCardDescription,
-    // this.type = HelpWidgetType.one,
-    // this.position = Alignment.bottomLeft,
+    // required this.controller,
   }) : super(key: key);
 
   @override
@@ -42,33 +60,41 @@ class HelpWidget extends StatefulWidget {
 class _HelpWidgetState extends State<HelpWidget> {
   bool _isPositiveDirection = false;
 
-  Widget? _mainCardWidget;
-  Widget? _flagWidget;
   Widget? _switchWidget;
-  Widget? _optionsWidget;
 
-  void _onClose() {
-    setState(() {
-      _switchWidget = _flagWidget;
-    });
+  Widget? _mainCardWidget;
+  Widget? _flagCardWidget;
+  Widget? _optionsCardWidget;
+
+  _MainBuilderController _mainBuilderController() {
+    final controller = _MainBuilderController();
+    controller.onClose = () {
+      _isPositiveDirection = false;
+      setState(() => _switchWidget = _flagCardWidget);
+    };
+    controller.onDetails = () {
+      _isPositiveDirection = true;
+      setState(() => _switchWidget = _optionsCardWidget);
+    };
+
+    return controller;
   }
 
-  void _onFlagWidgetPressed() {
+  _OptionsBuilderController _optionsBuilderController() {
+    final controller = _OptionsBuilderController();
+    controller.options = widget.options;
+    controller.onClose = () {
+      _isPositiveDirection = false;
+      setState(() {
+        _switchWidget = _mainCardWidget;
+      });
+    };
+
+    return controller;
+  }
+
+  void _onFlag() {
     _isPositiveDirection = true;
-    setState(() {
-      _switchWidget = _mainCardWidget;
-    });
-  }
-
-  void _onDetailsPressed() {
-    _isPositiveDirection = true;
-    setState(() {
-      _switchWidget = _optionsWidget;
-    });
-  }
-
-  void _onHidePressed() {
-    _isPositiveDirection = false;
     setState(() {
       _switchWidget = _mainCardWidget;
     });
@@ -78,26 +104,55 @@ class _HelpWidgetState extends State<HelpWidget> {
   void initState() {
     super.initState();
 
-    _flagWidget = const MouseRegion(
-      key: ValueKey(0),
-      cursor: SystemMouseCursors.click,
-      child: CardRounded(
-        child: _DefaultUkraineFlagWidget(),
+    _flagCardWidget = const _FlagCard();
+    _switchWidget = _flagCardWidget;
+
+    _mainCardWidget = widget.mainCardBuilder(_mainBuilderController());
+
+    _optionsCardWidget = widget.optionsCardBuilder(_optionsBuilderController());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _switchWidget == _flagCardWidget ? _onFlag : null,
+      behavior: HitTestBehavior.opaque,
+      child: _AnimationBuilder(
+        axis: widget.axis,
+        isPositiveDirection: _isPositiveDirection,
+        child: _switchWidget,
       ),
     );
-
-    _mainCardWidget = widget.mainCardBuilder(
-      const _DefaultUkraineFlagWidget(),
-      widget.mainCardTitle,
-      widget.mainCardDescription,
-      _onDetailsPressed,
-      _onClose,
-    );
-
-    _optionsWidget = widget.optionsCardBuilder(widget.options, _onHidePressed);
-
-    _switchWidget = _flagWidget;
   }
+}
+
+class _FlagCard extends StatelessWidget {
+  const _FlagCard({Key key = const ValueKey(0)}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: CardRounded(
+        child: DefaultUkraineFlagWidget(),
+      ),
+    );
+  }
+}
+
+class _AnimationBuilder extends StatelessWidget {
+  final Widget? child;
+
+  final Axis axis;
+
+  final bool isPositiveDirection;
+
+  const _AnimationBuilder({
+    Key? key,
+    required this.child,
+    required this.axis,
+    required this.isPositiveDirection,
+  }) : super(key: key);
 
   Widget _layoutBuilder(Widget? currentChild, List<Widget> _) {
     return currentChild ?? const SizedBox();
@@ -106,7 +161,7 @@ class _HelpWidgetState extends State<HelpWidget> {
   Widget _transitionBuilder(Widget child, Animation<double> animation) {
     const padding = EdgeInsets.all(12.0);
 
-    final _offsets = widget.axis == Axis.horizontal
+    final _offsets = axis == Axis.horizontal
         ? const [Offset(-0.5, 0.0), Offset(0.5, 0.0)]
         : const [Offset(0.0, -1.5), Offset(0.0, 1.5)];
 
@@ -120,7 +175,7 @@ class _HelpWidgetState extends State<HelpWidget> {
       end: Offset.zero,
     ).animate(animation);
 
-    return child.key == const ValueKey(0) || !_isPositiveDirection
+    return child.key == const ValueKey(0) || !isPositiveDirection
         ? SlideTransition(
             position: inAnimation,
             child: Padding(padding: padding, child: child),
@@ -133,37 +188,13 @@ class _HelpWidgetState extends State<HelpWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _switchWidget == _flagWidget ? _onFlagWidgetPressed : null,
-      behavior: HitTestBehavior.opaque,
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 400),
-        layoutBuilder: _layoutBuilder,
-        transitionBuilder: _transitionBuilder,
-        switchInCurve: Curves.fastOutSlowIn,
-        switchOutCurve: Curves.fastOutSlowIn,
-        child: _switchWidget,
-      ),
-    );
-  }
-}
-
-class _DefaultUkraineFlagWidget extends StatelessWidget {
-  const _DefaultUkraineFlagWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    const size = 51.0;
-    const borderRadius = 13.0;
-
-    return Container(
-      height: size,
-      width: size,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(borderRadius),
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: const FlagWidget(),
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 400),
+      layoutBuilder: _layoutBuilder,
+      transitionBuilder: _transitionBuilder,
+      switchInCurve: Curves.fastOutSlowIn,
+      switchOutCurve: Curves.fastOutSlowIn,
+      child: child,
     );
   }
 }
