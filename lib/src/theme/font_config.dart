67c627383f5eed847ef0cc0e0930201a70514943
Yurii Prykhodko <yurii.prykhodko@solid.software>
2022-09-01 13:56:56 +0300

    Fix Roboto font loading from library

--- lib/src/theme/font_config.dart
+++ lib/src/theme/font_config.dart
@@ -1,5 +1,6 @@
 /// global configurations regarding font
 class FontConfig {
   /// fontFamily
-  static const family = 'Roboto';
+  // static const family = 'Roboto';
+  static const family = 'packages/help_ukraine_widget/Roboto';
 }
